8ed27044eb679b733ee55da128abf5d1822b8e6c
tomito61 <112243546+tomito61@users.noreply.github.com>
2023-08-08 01:27:41 +0900

    Using LED available

--- lec_1/lec_1/joy_sub.py
+++ lec_1/lec_1/joy_sub.py
@@ -1,27 +1,11 @@
 #!/usr/bin/env python3
 
-# This example shows a simple way to control the Motoron Motor Controller.
-#
-# The motors will stop but automatically recover if:
-# - Motor power (VIN) is interrupted
-# - A temporary motor fault occurs
-# - A command timeout occurs
-#
-# This program will terminate if it does not receive an acknowledgment bit from
-# the Motoron for a byte it has written or if any other exception is thrown by
-# the underlying Python I2C library.
-#
-# The motors will stop until you restart this program if the Motoron
-# experiences a reset.
-#
-# If a latched motor fault occurs, the motors experiencing the fault will stop
-# until you power cycle motor power (VIN) or cause the motors to coast.
-
 import time
 import math
 import motoron
 import busio
 import board
+import RPi.GPIO as GPIO 
 import neopixel
 import adafruit_amg88xx
 import matplotlib.pyplot as plt
@@ -32,6 +16,12 @@ from rclpy.node import Node
 from sensor_msgs.msg import Joy
 
 mc = motoron.MotoronI2C()
+#ポート番号の定義
+Led_pin = 25                        #変数"Led_pin"に18を格納
+
+#GPIOの設定
+GPIO.setmode(GPIO.BCM)              #GPIOのモードを"GPIO.BCM"に設定
+GPIO.setup(Led_pin, GPIO.OUT)       #GPIO18を出力モードに設定
 
 # I2Cバスの初期化
 i2c_bus = busio.I2C(board.SCL, board.SDA)
@@ -56,19 +46,13 @@ mc.disable_crc()   # Bytes: 0x8B 0x04 0x7B 0x43
 # counts as an error.
 mc.clear_reset_flag()  # Bytes: 0xA9 0x00 0x04
 
-# By default, the Motoron is configured to stop the motors if it does not get
-# a motor control command for 1500 ms.  You can uncomment a line below to
-# adjust this time or disable the timeout feature.
-# mc.set_command_timeout_milliseconds(1000)
-# mc.disable_command_timeout()
-
 # Configure motor 1
 mc.set_max_acceleration(1, 200)
-mc.set_max_deceleration(1, 300)
+mc.set_max_deceleration(1, 200)
 
 # Configure motor 2
 mc.set_max_acceleration(2, 200)
-mc.set_max_deceleration(2, 300)
+mc.set_max_deceleration(2, 200)
 
 # Assigning parameter values
 ppr = 800  # Pulses Per Revolution of the encoder
@@ -77,8 +61,8 @@ tsample = 0.01  # Sampling period for code execution (s)
 tdisp = 0.5  # Sampling period for values display (s)
 
 # Creating encoder object using GPIO pins 24 and 25
-encoder1 = RotaryEncoder(13, 6, max_steps=0)
-encoder2 = RotaryEncoder(26, 19, max_steps=0)
+encoder1 = RotaryEncoder(6, 5, max_steps=0)
+encoder2 = RotaryEncoder(26, 16, max_steps=0)
 
 # Initializing previous values and starting main clock
 anglecurr = 0
@@ -88,10 +72,10 @@ tstart = time.perf_counter()
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
 # NeoPixels must be connected to D10, D12, D18 or D21 to work.
-pixel_pin = board.D18
+pixel_pin = board.D10
 
 # The number of NeoPixels
-num_pixels = 30
+num_pixels = 10
 
 # The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
 # For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
@@ -122,7 +106,6 @@ def wheel(pos):
          b = int(255 - pos * 3)
       return (r, g, b) if ORDER in (neopixel.RGB, neopixel.GRB) else (r, g, b, 0)
 
-
 def rainbow_cycle(wait):
    for j in range(255):
       for i in range(num_pixels):
@@ -136,9 +119,6 @@ class JoySubscriber(Node):
         super().__init__("joy_subscriber")
         self.subscription_joy = self.create_subscription(Joy, "/joy", self.joy_callback, 10)
         self.subscription_joy
-        
-
-   
     
    def joy_callback(self, joy_msg):  
       if(joy_msg.buttons[3]==1):    
@@ -153,18 +133,23 @@ class JoySubscriber(Node):
       global tr3,group,tprev
        
       if(joy_msg.buttons[1]==1):
+         pixels.fill((0, 0, 255))
          tr3=1
          group=0
        
       if(joy_msg.buttons[0]==1):
+         pixels.fill((0, 0, 0))
          tr3=0
          group=0
            
       if(joy_msg.buttons[2]==1):
+         GPIO.output(Led_pin, GPIO.HIGH)
+         pixels.fill((0, 0, 255))
          group=1
        
       if(tr3==1 or group==1):
-         rainbow_cycle(0.001)
+         pixels.fill((0, 0, 255))
+         time.sleep(0.001)
          right=-joy_msg.axes[1]
          left=joy_msg.axes[3]
        
@@ -172,9 +157,8 @@ class JoySubscriber(Node):
          left_duty=int(left*1000)
        
          mc.set_speed(1, -right_duty)
-
          mc.set_speed(2, -left_duty)
-         time.sleep(0.005)
+         time.sleep(0.001)
            
       # Pausing for `tsample` to give CPU time to process encoder signal
       time.sleep(tsample)
@@ -191,9 +175,12 @@ class JoySubscriber(Node):
          print("Dis_right = {:0.03f} deg Angle_right = {:0.03f} deg".format(distance1,anglecurr1))
       # Updating previous values
       tprev = tcurr
+      
+      pixels.show()
 
 
 def main(args=None):
+    pixels.fill((0, 0, 0))
     rclpy.init(args=args)
     sub = JoySubscriber()
     rclpy.spin(sub)
@@ -201,17 +188,4 @@ def main(args=None):
 
 if __name__ == "__main__":
     main()
-#try:
-#  while True:
-#    if int(time.monotonic() * 1000) & 2048:
- #     mc.set_speed(1, 800)
-  #  else:
-   #   mc.set_speed(1, -800)
-#
- #   mc.set_speed(2, 100)
-    
 
-  #  time.sleep(0.005)
-
-#except KeyboardInterrupt:
- # pass
